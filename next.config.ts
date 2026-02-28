24910eaf5ba5a68b81246f3d7ad76688ee3b8ecc
Jimmy Lee <liji420@qq.com>
2026-02-28 10:51:58 +0800

    添加静态导出设置

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',  // 生成纯静态文件
+    images: {
+        unoptimized: true, // 禁用图片优化（静态站不支持）
+    },
 };
 
 export default nextConfig;
